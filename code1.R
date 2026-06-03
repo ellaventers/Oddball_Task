95e923354a25c9f390501471d1c71639697b24ba
ellaventers <emv5505@gmail.com>
2026-06-03 13:39:32 -0400

    Create code1.R
    first upload

---
+++ code1.R
@@ -0,0 +1,18 @@
+
+setwd("C:/Users/emv55/OneDrive/Documents/DataForLabs")
+library(tidyverse)
+cleanData <- read.csv("clean_data(1).csv")
+View(cleanData)
+sub1873B <- read.delim("1873_BL.txt")
+View(sub1873B)
+write.csv(sub1873B, "sub1873B.csv")
+sub1873B <- read.csv("sub1873B.csv")
+View(sub1873B)
+text_lines <- readLines("1873_BL.txt")
+numbers <- str_extract_all(text_lines, "\\d+")
+df <- data.frame(Numbers = unlist(numbers))
+data1 <- write.csv(df, "numbers_only.csv", row.names = FALSE)
+View(df)
+num_only <-read.csv("numbers_only.csv")
+View(num_only)
+
